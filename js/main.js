e597b2c1fc60d730ed52b18faa352d42c0b98c6d
Guilhermee <xpgui@outlook.com>
2023-07-02 06:15:12 -0300

    preparando ambiente

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#novoItem');
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    console.log(evento.target.elements["nome"].value)
+    console.log(evento.target.elements["quantidade"].value)
+})
