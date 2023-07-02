5e6f0863983de1ebc598ee70280c278669730eb2
Guilhermee <xpgui@outlook.com>
2023-07-02 07:15:58 -0300

    manipulando os dados

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const form = document.querySelector('#novoItem');
+const lista = document.querySelector("#lista");
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+    evento.preventDefault();
 
-    console.log(evento.target.elements["nome"].value)
-    console.log(evento.target.elements["quantidade"].value)
+    criarElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value);
 })
+
+function criarElemento(nome, quantidade) {
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement("strong");
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+
+    console.log(novoItem);
+}
